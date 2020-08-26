c7cb36596aa03d1e2b7798b52a4ce15f2a295a11
Eduardo  Flores Coppede <eduardofc@gmail.com>
2020-08-25 22:06:47 -0400

    Se agregan cambios necesarios para la implementación del algoritmo

--- kruskal.py
+++ kruskal.py
@@ -1,6 +1,12 @@
 class Kruskal:
 
-
     def __init__(self):
-        self.nodes = []
-        self.order = []
+        self.nodes = {}
+        self.order = {}
+
+    def prepare_structure(self, node): 
+        self.nodes[node] = node # {2: 2, 5: 5}
+        self.order[node] = 0 # {2: 0, 5:0}
+
+    def verify_union(self, origin, destination):
+        pass    
